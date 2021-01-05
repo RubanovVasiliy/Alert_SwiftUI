6fe22c48503b20290e9143ed7cc719048efce41c
Vasiliy <rubanovas2005@gmail.com>
2021-01-05 13:51:48 +0700

    Initial Commit

---
+++ Alert SwiftUI/Alert_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alert_SwiftUIApp.swift
+//  Alert SwiftUI
+//
+//  Created by macos on 05.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Alert_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
